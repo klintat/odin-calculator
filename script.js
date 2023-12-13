294b316cb46e0141bf8398e17a7f42f4b7e52205
klintat <98770096+klintat@users.noreply.github.com>
2023-12-13 23:56:01 +0200

    created basic math functions for operators and operate function

---
+++ script.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+    return a + b
+}
+
+function substract(a, b) {
+    return a - b
+}
+  
+function multiply(a, b) {
+    return a * b
+}
+  
+function divide(a, b) {
+    return a / b
+}
+
+function operate(operator, a, b) {
+    a = Number(a) // a can be any number
+    b = Number(b)
+    switch (operator) {
+      case '+': return add(a, b)
+      case '-': return substract(a, b)
+      case '*': return multiply(a, b)
+      case '/':
+        if (b === 0) return null
+        else return divide(a, b)
+      default: return null
+    }
+}
